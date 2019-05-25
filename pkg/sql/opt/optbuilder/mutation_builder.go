a1312578abff9f30e62919f61ce314ef0f356ce1
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-05-25 13:27:45 +0200

    sql,*: use error constructors from the errors package directly
    ... instead of the aliased definitions in `pgerror`.
    
    (This change is entirely mechanical, using perl)
    
    Release note: None

--- pkg/sql/opt/optbuilder/mutation_builder.go
+++ pkg/sql/opt/optbuilder/mutation_builder.go
@@ -784,7 +784,7 @@ func getAliasedTableName(n tree.TableExpr) (*tree.TableName, *tree.TableName) {
 	}
 	tn, ok := n.(*tree.TableName)
 	if !ok {
-		panic(pgerror.Unimplemented(
+		panic(errors.Unimplemented(
 			"complex table expression in UPDATE/DELETE",
 			"cannot use a complex table name with DELETE/UPDATE"))
 	}
